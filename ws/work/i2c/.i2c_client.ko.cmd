ce215a56489f8c7d76c9423fc992a93940b4871e
saipranay <ps.juluri@globalsoft.com>
2018-04-11 19:12:09 +0530

    hard work space

---
+++ ws/work/i2c/.i2c_client.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/juluri/work/i2c/i2c_client.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/juluri/work/i2c/i2c_client.ko /home/juluri/work/i2c/i2c_client.o /home/juluri/work/i2c/i2c_client.mod.o ;  true
---
+++ ws/work/i2c/.tmp_versions/i2c_client.mod
@@ -0,0 +1,3 @@
+/home/juluri/work/i2c/i2c_client.ko
+/home/juluri/work/i2c/i2c_client.o
+
